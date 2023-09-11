23f637a7e313ceed5faea186f7a0f026f2e12999
JesseChen <jesse_002@163.com>
2023-09-11 16:07:14 +0800

    executable

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include "./src/Token.hpp"
+#include "./src/Parser.hpp"
+#include "./src/JIT.hpp"
+int main(int argc, char ** argv) {
 
-int main(int, char **) { std::cout << "Hello, from pascal_jit_compiler!\n"; }
+if(argc != 2U) {
+    return 1; 
+}
+
+char* pascal = argv[1];
+
+    std::ifstream t(pascal);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    buffer << EOF;
+
+    Tokenizer tokenizer(buffer.str());
+    Parser parser(tokenizer, std::make_shared<Runtime>());
+    auto program = parser.parse();
+
+    auto fun = createJit(program->codegen());
+    fun();
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Pascal JIT Compiler
+### Build
+`cmake -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build`
+### Run Demo
+`./build/pascal_jit_compiler ./tests/cases/helloworld.pas`
+### Limitation
+> Only support ARM 64 platform now
