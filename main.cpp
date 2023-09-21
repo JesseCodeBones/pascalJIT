9eaff6f9b204fc4b86513f63d18cb019411062cb
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-09-21 11:27:18 +0800

    fix main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
+#include "./src/JIT.hpp"
+#include "./src/Parser.hpp"
+#include "./src/Token.hpp"
 #include <filesystem>
 #include <fstream>
-#include "./src/Token.hpp"
-#include "./src/Parser.hpp"
-#include "./src/JIT.hpp"
-int main(int argc, char ** argv) {
+#include <iostream>
+int main(int argc, char **argv) {
 
-if(argc != 2U) {
-    return 1; 
-}
+  if (argc != 2U) {
+    return 1;
+  }
 
-char* pascal = argv[1];
+  char *pascal = argv[1];
 
-    std::ifstream t(pascal);
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-    buffer << EOF;
+  std::ifstream t(pascal);
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+  Tokenizer tokenizer(buffer.str());
+  Parser parser(tokenizer, std::make_shared<Runtime>());
+  parser.debug = true;
+  auto program = parser.parse();
 
-    Tokenizer tokenizer(buffer.str());
-    Parser parser(tokenizer, std::make_shared<Runtime>());
-    auto program = parser.parse();
-
-    auto fun = createJit(program->codegen());
-    fun();
+  auto fun = createJit(program->codegen());
+  fun();
 }
