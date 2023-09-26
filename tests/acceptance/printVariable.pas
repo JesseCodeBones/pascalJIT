3ba4c9948c0b537d947e42ac22777eff8870a764
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-09-26 14:07:03 +0800

    more test

--- tests/acceptance/printVariable.pas
+++ tests/acceptance/printVariable.pas
@@ -1,7 +1,11 @@
 
 program HelloWorld; 
  var myInt:Integer = -42;
+ var myIntTwo:Integer = 42;
+ var myString: String = 'this is a string literal';
 begin    
  writeln(myInt);
+ writeln(myIntTwo);
+ writeln(myString);
 end.
 
